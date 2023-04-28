29fbf9b17995eadce613d0dcd30830f15e13ba41
marcusmuntean <xhm3ws@virginia.edu>
2023-04-28 13:28:28 -0400

    began hourly

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -13,7 +13,8 @@ function App() {
 
 function Coordinates() {
   const [zip, setZip] = useState();
-  const [jsonData, setJsonData] = useState();
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
 
   const API_Key = process.env.REACT_APP_api_key;
   const url = new URL("http://api.openweathermap.org/geo/1.0/zip");
@@ -25,10 +26,12 @@ function Coordinates() {
     url.searchParams.append("zip", zip);
     url.searchParams.append("appid", API_Key);
 
-    alert(API_Key);
     fetch(url)
       .then((result) => result.json())
-      .then((data) => setJsonData(data.results));
+      .then((data) => {
+        setLatitude(data.lat);
+        setLongitude(data.lon);
+      });
   };
 
   const change = (event) => {
@@ -39,7 +42,59 @@ function Coordinates() {
     <>
       <input value={zip} onChange={change} />
       <button onClick={() => HandleClick()}>Submit</button>
-      <p>Data is here: {jsonData}</p>
+      <p>latitude: {latitude}</p>
+      <p>longitude: {longitude}</p>
+      <Weather lat={latitude} lon={longitude} />
+    </>
+  );
+}
+
+function Weather(props) {
+  const [currentObj, setCurrentObj] = useState({});
+  const [weatherObj, setWeatherObj] = useState({});
+  const [hourlyArr, setHourlyArr] = useState([{}]);
+
+  const API_Key = process.env.REACT_APP_api_key;
+  const url = new URL("https://api.openweathermap.org/data/2.5/onecall");
+
+  const HandleClick = () => {
+    url.searchParams.delete("lat");
+    url.searchParams.delete("lon");
+    url.searchParams.delete("appid");
+
+    url.searchParams.append("lat", props.lat);
+    url.searchParams.append("lon", props.lon);
+    url.searchParams.append("appid", API_Key);
+
+    fetch(url)
+      .then((result) => result.json())
+      .then((data) => {
+        setCurrentObj(data.current);
+        setWeatherObj(data.current.weather[0]);
+        setHourlyArr(data.hourly);
+      });
+  };
+
+  return (
+    <>
+      <button onClick={() => HandleClick()}>Show Weather</button>
+      <h1>Current Weather: {weatherObj.main}</h1>
+      <h2>Description: {weatherObj.description}</h2>
+      <h3>Temperature: {currentObj.temp}</h3>
+      <h3>Feels like: {currentObj.feels_like}</h3>
+      <h3>Clouds: {currentObj.clouds}</h3>
+      <h3>Wind Speed: {currentObj.wind_speed}</h3>
+
+      <h1>Next Day Hourly Forecast</h1>
+      <Hourly hourlyArr={hourlyArr} />
+    </>
+  );
+}
+
+function Hourly(props) {
+  return (
+    <>
+      <p>Hourly: {props.hourlyArr[0].pressure}</p>
     </>
   );
 }
